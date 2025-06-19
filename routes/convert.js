7f8727d144cec511491738b473bf8b63131b7325
ALEXA8596 <alexjunyoung+github@gmail.com>
2025-06-18 22:40:12 -0700

    Added CSS, Added Song Selector
    Project Really Coming Out Well!

--- routes/convert.js
+++ routes/convert.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var fetch = require("node-fetch");
 const { getData } = require("spotify-url-info")(fetch);
 const { search } = require("../search/search.js");
-const { v4 } = require('uuid');
-const fs = require('fs');
-const path = require('path');
+const { v4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
 // Use a JSON file as a database
-const dbFilePath = path.join(__dirname, 'db.json');
+const dbFilePath = path.join(__dirname, "db.json");
 
 // Initialize the database file if it doesn't exist
 if (!fs.existsSync(dbFilePath)) {
@@ -18,7 +18,7 @@ if (!fs.existsSync(dbFilePath)) {
 // Helper functions to interact with the JSON database
 const db = {
   read: () => {
-    const data = fs.readFileSync(dbFilePath, 'utf-8');
+    const data = fs.readFileSync(dbFilePath, "utf-8");
     return JSON.parse(data);
   },
   write: (newData) => {
@@ -43,25 +43,58 @@ router.get("/", async function (req, res, next) {
           title: "Spotify Converter",
         });
       }
-      console.log(data);
+      // console.log(data);
       const trackList = await Promise.all(
         data.trackList
           ?.filter((track) => track.entityType == "track")
           .map(async (el) => {
+            let results = (await search(`${el.subtitle} - ${el.title}`))[
+              "tracks"
+            ];
+
+            if (!results || results.length === 0) {
+              console.log(
+                `${el.title} wasn't found the first time, retrying...`
+              );
+              results = (await search(`${el.title} - ${el.subtitle}`))[
+                "tracks"
+              ];
+            }
+            if (!results || results.length === 0) {
+              console.log(
+                `${el.title} wasn't found the second time, retrying...`
+              );
+              results = (await search(`${el.title} - ${el.subtitle}`))[
+                "tracks"
+              ];
+            }
+            if (!results || results.length === 0) {
+              console.log(
+                `${el.title} wasn't found the third time, giving up...`
+              );
+              results = [];
+            }
             return {
               title: el.title,
               artist: el.subtitle,
-              results: (await search(`${el.subtitle} - ${el.title}`))["tracks"],
+              results,
             };
           })
       );
 
+      // console.log(await JSON.stringify(trackList, null, 2))
+
       return res.render("convert", {
-        playlistData: trackList,
+        playlistData: {
+          trackList,
+          raw: data,
+        },
         playlistExists: true,
         title: "Spotify Converter",
       });
-    } catch {}
+    } catch (e) {
+      throw e;
+    }
   }
   res.render("convert", {
     playlistData: null,
@@ -74,7 +107,7 @@ router.get("/", async function (req, res, next) {
 router.post("/save", async function (req, res, next) {
   const playlistData = req.body;
 
-  if (!playlistData || !Array.isArray(playlistData)) {
+  if (!playlistData || !Array.isArray(playlistData.trackList)) {
     return res.status(400).json({ error: "Invalid playlist data" });
   }
 
@@ -103,35 +136,11 @@ router.get("/getPlaylistData/:playlistId", async function (req, res, next) {
   const existingData = await db.read();
 
   if (!playlistId || !existingData[playlistId]) {
-    console.log("Error")
+    console.log("Error");
     return res.status(404).json({ error: "Playlist not found" });
   }
 
   res.status(200).json({ playlistData: existingData[playlistId] });
-})
-
-// router.get("/callback", async function (req, res, next) {
-
-//   // Redirect the user to a page where they can initiate the playlist creation,
-//   // or automatically start the playlist creation process.
-//   // res.redirect(`/${state}/createYoutubePlaylist`); // Redirect to the create playlist page
-//   res.render('callback');
-// });
-
-// router.get("/getAccessToken", async function (req, res, next) {
-//   const playlistId = req.query.playlistId;
-
-//   if (!playlistId) {
-//     return res.status(400).send("Missing playlistId.");
-//   }
-
-//   const accessToken = db[playlistId].accessToken;
-
-//   if (!accessToken) {
-//     return res.status(404).send("Access token not found for this playlist.");
-//   }
-
-//   res.status(200).json({ accessToken: accessToken });
-// });
+});
 
 module.exports = router;
--- search/search.js
+++ search/search.js
@@ -1,6 +1,7 @@
 const { PlayerSearchResult } = require("./types/types");
 const { Track } = require("./Structures/Track");
 const { Innertube, UniversalCache } = require("youtubei.js");
+const e = require("express");
 
 /** CODE TAKEN FROM DISCORD MUSIC PLAYER */
 /**
@@ -8,52 +9,76 @@ const { Innertube, UniversalCache } = require("youtubei.js");
  * @param {string|Track} query The search query
  * @returns {Promise<PlayerSearchResult>}
  */
-async function search(query) {
-  const innertube = await Innertube.create({ cache: new UniversalCache(true) });
-  const ytMusic = innertube.music;
-  console.log(query)
-  const videos = (await ytMusic.search(query, {
-    type: "song",
-  }))["contents"][0]["contents"];
+async function search(query, verbose = false) {
+  try {
+    const innertube = await Innertube.create({
+      cache: new UniversalCache(true),
+    });
+    const ytMusic = innertube.music;
+    //   console.log(query)
+    const data = await ytMusic.search(query, {
+      type: "song",
+    });
+    //   const videos = data["contents"][0]["contents"];
+    const musicShelf = data["contents"].filter(
+      (el) =>
+        el.type == "MusicShelf" &&
+        el.title &&
+        Array.isArray(el.title.runs) &&
+        el.title.runs.some((run) => run.text == "Songs")
+    )[0];
 
-//   console.log(query);
+    const videos = musicShelf?.contents;
 
-//   fs.writeFileSync('data.json', JSON.stringify(videos, null, 2))
+    //   console.log(query);
 
-//   console.log(JSON.stringify(videos, null, 2))
-//   return videos
+    //   fs.writeFileSync('data.json', JSON.stringify(videos, null, 2))
 
-  if (!videos || videos.length === 0) {
+    //   console.log(JSON.stringify(videos, null, 2));
+
+      if(verbose) require("fs").writeFileSync("data.json", JSON.stringify(data, null, 2));
+
+    //   return videos
+
+    if (!videos || videos.length === 0) {
+      return { playlist: null, tracks: [] };
+    }
+
+    const tracks = videos.map((m) => {
+      // console.log(JSON.stringify(m))
+      return new Track({
+        title: m.title,
+        description: m.description || "",
+        author: m.artists.map((artist) => artist.name).join(", "),
+        url: m.url || `https://music.youtube.com/watch?v=${m.id}`,
+        id: m.id,
+        requestedBy: null,
+        thumbnail:
+          m?.thumbnail?.contents?.sort(
+            (a, b) => a.height * a.width > b.height * b.width
+          )[0].url || "",
+        views: m.views || 0,
+        duration: m.duration.text || "0:00",
+        source: "youtube-music",
+        album: {
+          name: m.album.name,
+          id: m.album.id,
+        },
+        raw: m,
+      });
+    });
+
+    return { playlist: null, tracks };
+  } catch (e) {
+    console.log(e);
     return { playlist: null, tracks: [] };
   }
-
-  const tracks = videos.map((m) => {
-    return new Track({
-      title: m.title,
-      description: m.description || "",
-      author: m.artists.map((artist) => artist.name).join(", "),
-      url: m.url || `https://music.youtube.com/watch?v=${m.id}`,
-      id: m.id,
-      requestedBy: null,
-      thumbnail: m?.thumbnail?.contents?.sort( (a,b) => a.height * a.width > b.height * b.width)[0].url || "",
-      views: m.views || 0,
-      duration: m.duration.text || "0:00",
-      source: "youtube-music",
-      album: {
-        name: m.album.name,
-        id: m.album.id
-      },
-      raw: m,
-    });
-  });
-
-  return { playlist: null, tracks };
 }
 
 // If the file is run directly, run the function
 if (require.main === module) {
-  const query = "Gracie Abrams tehe"
-  search(query).then((res) => {
+  const query = "Taylor Swift - Champagne Problems";
+  search(query, true).then((res) => {
     console.log(res);
   });
 }
